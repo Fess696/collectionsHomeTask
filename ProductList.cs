3e14e6606678c47d3ceeb7671648b1e9c2f2048e
Fess696 <75900532+Fess696@users.noreply.github.com>
2022-11-02 15:53:06 +0200

    Add files via upload
    ProductList class with properties which I want to parse from a products.json file

---
+++ ProductList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+using JsonSeDe;
+using System.Collections.Generic;
+
+namespace JsonSeDe
+{
+    [Serializable]
+    public class ProductList 
+    {
+        public string? Id { get; set; }
+
+        public string  Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? CategoryId { get; set; }
+
+        public string? Factory { get; set; }
+
+        public string? Supplier { get; set; }
+
+        public decimal? Price { get; set; }
+        
+    }
+}
+
