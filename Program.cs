744c2bfae567c36a978dff05d40b31a8b79a353f
Fess696 <75900532+Fess696@users.noreply.github.com>
2022-11-02 15:54:28 +0200

    Add files via upload
    Program class where I deserialize a json file to .net object

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using JsonSeDe;
+using Newtonsoft.Json;
+
+class Program 
+{ 
+    static void Main(string[] args)
+    {
+        var products = GetProductLists();
+
+        if (products != null)
+        { 
+            for (int i = 0; i < products.Count; i++)
+            {
+                System.Diagnostics.Debug.Print(products[i].Id);
+	    
+	        }
+	    }
+    }
+    static List<ProductList> GetProductLists()
+    {
+        string fileName =
+            @"/Users/nik/Projects/JsonSeDe/JsonSeDe/Data/product.json";
+            
+            if(File.Exists(fileName))
+            {
+            var products =
+                JsonConvert.DeserializeObject<List<ProductList>>
+                (File.ReadAllText(fileName));
+
+            return products;
+	     
+	        }
+
+        return null;
+    }
+}
+
+
+
